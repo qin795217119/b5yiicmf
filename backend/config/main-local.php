ca9ad26b59e3901cfc331a1bd5a5301d6490147c
冰舞网络 <357145480@qq.com>
2021-01-19 14:08:32 +0800

    ''

---
+++ backend/config/main-local.php
@@ -0,0 +1,58 @@
+<?php
+
+$config = [
+    'layout'=>'layout',
+    'defaultRoute' => 'index',
+    'runtimePath' => '@approot/runtime/admin',
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'mPMN3JUvLg6zFbz6abd5MmBuBtI9jE1C',
+        ],
+        'assetManager' => [
+            'basePath' => '@approot/public/assets/admin',
+            'baseUrl' => '@appweb/public/assets/admin',
+            'bundles' => [
+                'yii\web\JqueryAsset' => [
+                    'js'=>[],
+                    'sourcePath' => null,
+                ],
+                'yii\web\YiiAsset' => [
+                    'js' => [],  // 去除 yii.js
+                    'sourcePath' => null,  // 防止在 frontend/web/asset 下生产文件
+                ],
+                'yii\widgets\ActiveFormAsset' => [
+                    'js' => [],  // 去除 yii.activeForm.js
+                    'sourcePath' => null,  // 防止在 frontend/web/asset 下生产文件
+                ],
+                'yii\validators\ValidationAsset' => [
+                    'js' => [],  // 去除 yii.validation.js
+                    'sourcePath' => null,  // 防止在 frontend/web/asset 下生产文件
+                ],
+                'yii\bootstrap\BootstrapAsset' => [
+                    'css' => [],  // 去除 bootstrap.css
+                    'sourcePath' => null, // 防止在 frontend/web/asset 下生产文件
+                ],
+                'yii\bootstrap\BootstrapPluginAsset' => [
+                    'js' => [],  // 去除 bootstrap.js
+                    'sourcePath' => null,  // 防止在 frontend/web/asset 下生产文件
+                ],
+            ],
+        ],
+    ]
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
---
+++ common/config/main-local.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=b5yiicmf',
+            'username' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8',
+            'enableSchemaCache' => false,
+            // Duration of schema cache.
+            'schemaCacheDuration' => -1,
+            // Name of the cache component used to store schema information
+            'schemaCache' => 'cache',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
