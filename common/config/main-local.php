79b7460b35df0b68df57efe754c1ec1ee6b2e2a6
冰舞网络 <357145480@qq.com>
2022-03-30 21:32:41 +0800

    1

---
+++ common/config/main-local.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=b5yii2cmf',
+            'username' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8',
+        ],
+    ],
+];
