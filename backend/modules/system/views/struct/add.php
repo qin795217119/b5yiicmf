1efaa7e38b8b0a64128abff9448ddfaeb4bad16e
冰舞网络 <357145480@qq.com>
2022-05-23 20:37:55 +0800

    状态显示

--- backend/modules/system/views/struct/add.php
+++ backend/modules/system/views/struct/add.php
@@ -40,10 +40,10 @@
         <label class="col-sm-3 control-label is-required">状态：</label>
         <div class="col-sm-8">
             <label class="radio-box">
-                <input type="radio" name="status" value="0"/> 有效
+                <input type="radio" name="status" value="0"/> 无效
             </label>
             <label class="radio-box">
-                <input type="radio" name="status" value="1" checked/> 无效
+                <input type="radio" name="status" value="1" checked/> 有效
             </label>
         </div>
     </div>
